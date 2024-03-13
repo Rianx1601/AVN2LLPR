42ee2e730def4c486d14f76447096bf89d33bd68
Rianx1601 <115299227+Rianx1601@users.noreply.github.com>
2024-03-13 14:25:39 -0300

    Add files via upload

---
+++ matriz.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(){
+	int x=0, i, j;
+	int mat[3][5];
+				  
+	  for(  i=0 , i=<3 , i++ ){
+	  	for( j=0 , j<6 , j++){
+	  			mat[i][j]=(i*j+4);
+	  		
+	  		
+	  		if(mat{i}[j]=>15 && mat[i][j]=<20 ){
+	  			x=x+1
+			  }
+	  		
+		  }
+	  }
+	printf("%d",x);
+	return 0;
+}
